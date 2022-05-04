6e935a14a6e09de1481dc811535a5f05d2b5a598
uwo-o <hugo.campos@usm.cl>
2022-05-04 02:32:13 -0400

    basic structure implemented

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-import flask
+# We import the following modules:
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+# This route gets a JSON file from the frontend with the parameters to create the MD
+@app.route('/create_md', methods=['POST'])
+def create_md():
+    # We get the JSON file from the frontend.
+    data = request.get_json()
+
+if __name__ == '__main__':
+    app.run(debug=True)
